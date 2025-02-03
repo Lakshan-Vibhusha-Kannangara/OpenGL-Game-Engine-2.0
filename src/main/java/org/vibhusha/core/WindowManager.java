e66716c03133ceef27eed39285dd71ab107a67e9
vibhushakannangara <vibhooshakannangara@gmail.com>
2025-02-03 21:54:12 +0530

    Initial Commit

---
+++ src/main/java/org/vibhusha/core/WindowManager.java
@@ -0,0 +1,62 @@
+package org.vibhusha.core;
+
+import org.joml.Matrix4f;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.system.MemoryUtil;
+
+public class WindowManager {
+
+    private String title;
+    private int width;
+    private int height;
+    private boolean vSync;
+    private long window;
+
+    private static final float Z_NEAR = 0.01f;
+    private static final float Z_FAR = 1000f;
+    private final Matrix4f projectionMatrix;
+
+    public WindowManager(String title, int width, int height, boolean vSync) {
+        this.title = title;
+        this.width = width;
+        this.height = height;
+        this.vSync = vSync;
+        this.projectionMatrix = new Matrix4f();
+    }
+
+    public void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if (!GLFW.glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+        GLFW.glfwDefaultWindowHints();
+        GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GL11.GL_FALSE);
+        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 3);
+        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 2);
+        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
+        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GL11.GL_TRUE);
+
+        boolean maximised = false;
+        if (width == 0 || height == 0) {
+            width = 100;
+            height = 100;
+            GLFW.glfwWindowHint(GLFW.GLFW_MAXIMIZED, GLFW.GLFW_TRUE);
+            maximised = true;
+        }
+        window = GLFW.glfwCreateWindow(width, height, title, MemoryUtil.NULL, MemoryUtil.NULL);
+        if (window == MemoryUtil.NULL) {
+            throw new RuntimeException("Failed to create GLFW Window");
+        }
+        GLFW.glfwSetFramebufferSizeCallback(window,(window,width,height) ->{
+           this.width = width;
+           this.height= height;
+           this.setResize(true);
+        });
+    }
+
+    private void setResize(boolean b) {
+    }
+}
