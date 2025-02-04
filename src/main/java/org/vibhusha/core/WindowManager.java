2b38fd31564b10d6c13a675dd5f829b749efb025
vibhushakannangara <vibhooshakannangara@gmail.com>
2025-02-04 18:55:28 +0530

    Added WindowManager.java file

--- src/main/java/org/vibhusha/core/WindowManager.java
+++ src/main/java/org/vibhusha/core/WindowManager.java
@@ -1,103 +1,71 @@
 package org.vibhusha.core;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.joml.Matrix4f;
+import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWErrorCallback;
-import org.lwjgl.system.MemoryUtil;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
+
+import java.awt.Desktop;
+import java.net.URI;
 
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11C.*;
+import static org.lwjgl.system.MemoryUtil.*;
 
 public class WindowManager {
-
-    private String title;
-    private int width;
-    private int height;
-    private boolean vSync;
     private long window;
-    public static final float FOV = (float) Math.toRadians(60);
-    private static final float Z_NEAR = 0.01f;
-    private static final float Z_FAR = 1000f;
-    private final Matrix4f projectionMatrix;
+    private int width = 800;
+    private int height = 600;
+    private String title = "OpenGL Window";
 
-    @Getter
-    private boolean resize;
-
-    public WindowManager(String title, int width, int height, boolean vSync) {
-        this.title = title;
-        this.width = width;
-        this.height = height;
-        this.vSync = vSync;
-        this.projectionMatrix = new Matrix4f();
+    public void run() {
+        init();
+        loop();
+        glfwDestroyWindow(window);
+        glfwTerminate();
     }
 
-    public void init() {
+    private void init() {
         GLFWErrorCallback.createPrint(System.err).set();
 
         if (!glfwInit()) {
             throw new IllegalStateException("Unable to initialize GLFW");
         }
 
-        glfwDefaultWindowHints();
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-        boolean maximised = false;
-        if (width == 0 || height == 0) {
-            width = 100;
-            height = 100;
-            glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
-            maximised = true;
+        window = glfwCreateWindow(width, height, title, NULL, NULL);
+        if (window == NULL) {
+            throw new RuntimeException("Failed to create GLFW window");
         }
 
-        window = glfwCreateWindow(width, height, title, MemoryUtil.NULL, MemoryUtil.NULL);
-        if (window == MemoryUtil.NULL) {
-            throw new RuntimeException("Failed to create GLFW Window");
+        glfwMakeContextCurrent(window);
+        glfwShowWindow(window);
+        GL.createCapabilities();
+    }
+
+    private void loop() {
+        while (!glfwWindowShouldClose(window)) {
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+
+            if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS) {
+                openBrowser("https://www.google.com");
+            }
         }
+    }
 
-        glfwMakeContextCurrent(window); // Ensures OpenGL context is set
-        if (vSync) {
-            glfwSwapInterval(1); // Enables vSync
+    private void openBrowser(String url) {
+        if (Desktop.isDesktopSupported()) {
+            try {
+                Desktop.getDesktop().browse(new URI(url));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        glfwShowWindow(window); // Show the window
-
-        glfwSetFramebufferSizeCallback(window, (win, w, h) -> {
-            this.width = w;
-            this.height = h;
-            this.setResize(true);
-        });
     }
 
-    public void update() {
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
 
-    public void setClearColour(float r, float g, float b, float a) {
-        glClearColor(r, g, b, a);
-    }
-
-    public boolean isKeyPressed(int keycode) {
-        return glfwGetKey(window, keycode) == GLFW_PRESS;
-    }
-
-    public boolean windowShouldClose() {
-        if (window == 0) {
-            throw new IllegalStateException("GLFW window not initialized");
-        }
-        return glfwWindowShouldClose(window);
-    }
-
-    public Matrix4f updateProjectionMatrix() {
-        float aspectRatio = (float) width / height;
-        return projectionMatrix.setPerspective(FOV, aspectRatio, Z_NEAR, Z_FAR);
-    }
-
-    public void setResize(boolean resize) {
-        this.resize = resize;
-    }
 }
--- src/main/java/org/vibhusha/test/Launcher.java
+++ src/main/java/org/vibhusha/test/Launcher.java
@@ -3,13 +3,14 @@ package org.vibhusha.test;
 import org.lwjgl.Version;
 import org.vibhusha.core.WindowManager;
 
+
+import javax.swing.*;
+
 public class Launcher {
     public static void main(String[] args) {
-        System.out.println(Version.getVersion());
-
-        WindowManager windowManager = new WindowManager("Game Engine",1600,900,false);
-        while(!windowManager.windowShouldClose()){
-            windowManager.init();
-        }
+        new WindowManager().run();
     }
+
 }
+
+
